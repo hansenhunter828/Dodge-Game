ec287da0693f4617a3917422121a5c12b11e8017
Hunter Hansen <hunthans828@ed.amdsb.ca>
2021-03-21 16:57:59 -0400

    Add project files.

---
+++ Dodge Game/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Dodge_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(250, 158);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(139, 23);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.outputLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dodge Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Dodge Game/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodge_Game
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        #region Player Variables
+        int playerX = 10;
+        int playerY = 10;
+
+        int paddleHeight = 20;
+        int paddleWidth = 20;
+        int paddleSpeed = 6;
+        #endregion
+        #region Player Bools
+        bool wDown = false;
+        bool aDown = false;
+        bool sDown = false;
+        bool dDown = false;
+        #endregion
+        #region Obstacle Variables And Lists
+        List<int> leftObstacleYList = new List<int>();
+        List<int> rightObstacleYList = new List<int>();
+        int obstacleHeight = 50;
+        int obstacleWidth = 10;
+        int obstacleSpeed = 6;
+
+        int movingObstacleCounter = 0;
+        int flashObstacleCounter = 0;
+        #endregion
+        #region Brushes
+        SolidBrush pinkBrush = new SolidBrush(Color.Pink);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        #endregion
+        int counter = 0;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            leftObstacleYList.Add(0);
+            rightObstacleYList.Add(275);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                #region Player Controls
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                    #endregion
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                #region Player Controls
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                    #endregion
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            flashObstacleCounter++;
+            movingObstacleCounter++;
+            #region Player Movement
+            if (wDown == true && playerY > 0)
+            {
+                playerY -= paddleSpeed;
+            }
+            if (sDown == true && playerY < this.Height)
+            {
+                playerY += paddleSpeed;
+            }
+            if (aDown == true && playerY > 0)
+            {
+                playerX -= paddleSpeed;
+            }
+            if (dDown == true && playerY < this.Width)
+            {
+                playerX += paddleSpeed;
+            }
+            #endregion
+            #region Obstacles
+            // spawn obstacles
+            counter++;
+
+            if (movingObstacleCounter == 40)
+            {
+                leftObstacleYList.Add(0);
+                rightObstacleYList.Add(275);
+                movingObstacleCounter = 0;
+            }
+            #endregion
+            #region Obstacle Movement
+            // obstacle movement
+
+            for (int i = 0; i < leftObstacleYList.Count(); i++)
+            {
+                leftObstacleYList[i] += obstacleSpeed;
+            }
+            for (int i = 0; i < rightObstacleYList.Count(); i++)
+            {
+                rightObstacleYList[i] -= obstacleSpeed;
+            }
+            #endregion
+            #region Obstacle deletetion
+            //obstacle delete
+            for (int i = 0; i < leftObstacleYList.Count(); i++)
+            {
+                if (leftObstacleYList[i] > this.Height)
+                {
+                    leftObstacleYList.RemoveAt(i);
+                }
+            }
+            for (int i = 0; i < rightObstacleYList.Count(); i++)
+            {
+                if (rightObstacleYList[i] < 0)
+                {
+                    rightObstacleYList.RemoveAt(i);
+                }
+            }
+            #endregion
+            #region Player And Obstacle Collisions
+            // player obstacle collisios
+            Rectangle playerRec = new Rectangle(playerX, playerY, paddleWidth, paddleHeight);
+
+            #region Obstacle 1
+            for (int i = 0; i < leftObstacleYList.Count(); i++)
+            {
+                Rectangle leftObstacle = new Rectangle(100, leftObstacleYList[i], obstacleWidth, obstacleWidth);
+
+                if (playerRec.IntersectsWith(leftObstacle))
+                {
+                    GameOver();
+                }
+            }
+            #endregion
+            #region Obstacle 2
+            for (int i = 0; i < rightObstacleYList.Count; i++)
+            {
+                Rectangle rightObstacle = new Rectangle(275, rightObstacleYList[i], obstacleWidth, obstacleWidth);
+
+                if (playerRec.IntersectsWith(rightObstacle))
+                {
+                    GameOver();
+                }
+            }
+            #endregion
+            #region Obstacle 3
+            Rectangle flashBarRec = new Rectangle(375, 0, 10, this.Height);
+            if (flashObstacleCounter >= 20 && flashObstacleCounter <= 40)
+            {
+                if (playerRec.IntersectsWith(flashBarRec))
+                {
+                    GameOver();
+                }
+                flashObstacleCounter = 0;
+            }
+            #endregion
+            #endregion
+            #region Player Win
+            //player wins
+            if (playerX >= this.Width - paddleHeight)
+            {
+                outputLabel.Text = "You Won";
+                gameTimer.Enabled = false;
+            }
+            #endregion
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            #region Draw Player
+            // Player
+            e.Graphics.FillRectangle(pinkBrush, playerX, playerY, paddleWidth, paddleHeight);
+            #endregion
+            #region Draw Obstacles
+            //Obstacles
+            //Obstacle 1
+            for (int i = 0; i < leftObstacleYList.Count; i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, 100, leftObstacleYList[i], obstacleWidth, obstacleHeight);
+            }
+            //Obstacle
+            for(int i = 0; i < rightObstacleYList.Count; i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, 275, rightObstacleYList[i], obstacleWidth, obstacleHeight);
+            }
+            //Obstaacle 3
+            if (flashObstacleCounter >= 20 && flashObstacleCounter <= 40)
+            {
+                e.Graphics.FillRectangle(blackBrush, 375, 0, 10, this.Height);
+                flashObstacleCounter = 0;
+            }
+            else
+            {
+                e.Graphics.FillRectangle(whiteBrush, 375, 0, 10, this.Height);
+            }
+            #endregion
+        }
+        public void GameOver()
+        {
+            outputLabel.Text = "You Lost";
+            gameTimer.Enabled = false;
+        }
+    }
+}
